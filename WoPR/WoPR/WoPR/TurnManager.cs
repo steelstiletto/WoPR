672a8947863a3a5c57dcd5a9c8d0d14e5d62c59b
steelstiletto <steel.stiletto@gmail.com>
2013-12-11 21:15:57 -0600

    TurnManager empty file

---
+++ WoPR/WoPR/WoPR/TurnManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace WoPR
+{
+    class TurnManager : Microsoft.Xna.Framework.GameComponent
+    {
+
+        public TurnManager(Game game)
+            : base(game)
+        {
+
+        }
+
+    }
+}
